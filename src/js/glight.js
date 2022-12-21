eb35ba45d9957f6efe09dd85ed07551f4d238a95
proflinervk <proflinervk@gmail.com>
2022-12-22 00:18:06 +0300

    add fancyBox JS

--- src/js/glight.js
+++ src/js/glight.js
@@ -1,16 +1,1 @@
-import GLightbox from 'glightbox'
-import { Zoom } from 'swiper';
-
-document.addEventListener( 'DOMContentLoaded', () => {
-	'use strict'
-
-} )
-
-const lightbox = GLightbox({
-    touchNavigation: true,
-    loop: true,
-	openEffect: Zoom,
-	closeEffect: Zoom,
-	closeButton: true,
-	closeOnOutsideClick: true,
-} )
+import { Fancybox } from "@fancyapps/ui";
