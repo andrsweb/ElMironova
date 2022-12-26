836d0379c7b0026071d880ab2e783a23d314dc80
proflinervk <proflinervk@gmail.com>
2022-12-26 04:16:59 +0300

    add examples section

---
+++ src/js/sliders/body-examples-swiper.js
@@ -0,0 +1,45 @@
+import Swiper, { Pagination, Navigation } from "swiper"
+
+document.addEventListener('DOMContentLoaded', () => {
+	'use strict'
+
+	initSwiper()
+})
+
+const initSwiper = () => {
+
+	const swiper = new Swiper('.body-swiper', {
+
+		direction: 'horizontal',
+		loop: true,
+		slidesPerView: 2,
+		spaceBetween: 12,
+
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			480: {
+				slidesPerView: 2,
+				spaceBetween: 12
+			},
+			767: {
+				slidesPerView: 3,
+			}
+		},
+
+		modules: [Pagination, Navigation],
+
+		pagination: {
+			el: '.body-pagination',
+			clickable: true
+		},
+
+		navigation: {
+			nextEl: '.body-next',
+			prevEl: '.body-prev'
+		}
+	})
+}
+
