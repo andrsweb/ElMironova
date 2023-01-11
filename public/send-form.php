4688fc7b7e818ad932d7a0f0c284e95d19876315
andrsweb <proflinervk@gmail.com>
2023-01-11 22:40:08 +0300

    correct email in php

--- public/send-form.php
+++ public/send-form.php
@@ -234,7 +234,7 @@ function as_send_email( string $subject, string $message ){
 		"Reply-To: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" .
 		"X-Mailer: PHP/" . phpversion();
 
-	$result = mail('Doc-mironova.ru@yandex.ru', $subject, $message, $headers );
+	$result = mail('doc-mironova.ru@yandex.ru', $subject, $message, $headers );
 
 	if( $result )
 		echo json_encode( [
